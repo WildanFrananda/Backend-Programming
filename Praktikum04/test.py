be14404d59c1c2e9e459be60e1abfc0bb41c63a7
Wildan Frananda <92700890+WildanFrananda@users.noreply.github.com>
2022-03-24 19:14:57 +0700

    Praktikum 4

---
+++ Praktikum04/test.py
@@ -0,0 +1,9 @@
+import math
+class PersegiPanjang:
+    def persegi_panjang (panjang, lebar):
+        luas = panjang * lebar
+        keliling = 2 * (panjang + lebar)    
+    # luas persegi panjang
+        print ("luas persegi panjang \t\t= %d" % luas)
+    # keliling persegi panjang
+        print ("keliling persegi panjang \t= %d" % keliling)
